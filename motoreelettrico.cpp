c26c6727d98ad3019eea0510de825d6af401fe1a
archetipo95 <martin.masevski@gmail.com>
2020-05-13 17:55:50 +0200

    add Metodi get e Costruttore

--- motoreelettrico.cpp
+++ motoreelettrico.cpp
@@ -1,6 +1,33 @@
 #include "motoreelettrico.h"
 
-motoreElettrico::motoreElettrico()
+bool motoreElettrico::getInCarica() const
+{
+    return inCarica;
+}
+
+double motoreElettrico::getCapacitaBatteria() const
+{
+    return capacitaBatteria;
+}
+
+double motoreElettrico::getAmpereBatteria() const
+{
+    return ampereBatteria;
+}
+
+u_int motoreElettrico::getCaricaSupportata() const
+{
+    return caricaSupportata;
+}
+
+u_int motoreElettrico::getVelocitaCarica() const
+{
+    return velocitaCarica;
+}
+
+motoreElettrico::motoreElettrico(string tar, string pos, u_int staA, float km, bool inR, bool ricM, u_int ing, u_int emi,
+                                 bool inC, double capB, double ampB, u_int carS, u_int velC):
+    veicolo(tar, pos, staA, km, inR, ricM, ing, emi), inCarica(inC), capacitaBatteria(capB), ampereBatteria(ampB), caricaSupportata(carS), velocitaCarica(velC)
 {
 
 }
--- motoreelettrico.h
+++ motoreelettrico.h
@@ -11,8 +11,20 @@ private:
     double ampereBatteria;          // Ampere-ora presenti
     u_int caricaSupportata;        // la massima velocità alla quale può essere ricaricata (da 1 a 4)
     u_int velocitaCarica;           // tipo di colonnina alla quale è inCarica (da 1 a 4)
+
 public:
-    motoreElettrico();
+    // Costruttore
+    motoreElettrico(string tar, string pos, u_int staA, float km, bool inR, bool ricM, u_int ing, u_int emi,
+                    bool inC, double capB, double ampB, u_int carS, u_int velC);
+
+    // Metodi virtuali
+
+    // Metodi get
+    bool getInCarica() const;
+    double getCapacitaBatteria() const;
+    double getAmpereBatteria() const;
+    u_int getCaricaSupportata() const;
+    u_int getVelocitaCarica() const;
 };
 
 #endif // MOTOREELETTRICO_H
