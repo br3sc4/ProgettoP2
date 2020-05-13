2a89eb6f4fade10aaf1db7647d6cf3457f0f0797
archetipo95 <martin.masevski@gmail.com>
2020-05-13 17:45:42 +0200

    add parte privata MotoreElettrico

--- motoreelettrico.h
+++ motoreelettrico.h
@@ -5,6 +5,12 @@
 
 class motoreElettrico : public veicolo
 {
+private:
+    bool inCarica;
+    double capacitaBatteria;        // Ampere-ora (Ah) massimi
+    double ampereBatteria;          // Ampere-ora presenti
+    u_int caricaSupportata;        // la massima velocità alla quale può essere ricaricata (da 1 a 4)
+    u_int velocitaCarica;           // tipo di colonnina alla quale è inCarica (da 1 a 4)
 public:
     motoreElettrico();
 };
