6be51782477b3e0817778902611340a684479a75
archetipo95 <martin.masevski@gmail.com>
2020-05-20 12:54:38 +0200

    valori default monopattino

--- Model/monopattino.h
+++ Model/monopattino.h
@@ -6,7 +6,7 @@
 class Monopattino: public MotoreElettrico
 {
 public:
-    Monopattino(string targa, string posizione, double km, u_int numeroPosti, u_int ingombro, double capacitaBatteria, VelocitaRicarica caricaSupportata);
+    Monopattino(string targa, string posizione, double km, double capacitaBatteria, VelocitaRicarica caricaSupportata, u_int numeroPosti=1, u_int ingombro=1);
 };
 
 #endif // MONOPATTINO_H
