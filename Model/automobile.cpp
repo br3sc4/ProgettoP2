98137c6acf49ee5cbf36185656e0a6558e6eb1be
archetipo95 <martin.masevski@gmail.com>
2020-05-20 12:44:22 +0200

    valori di default per automobile

--- Model/automobile.cpp
+++ Model/automobile.cpp
@@ -1,6 +1,3 @@
 #include "automobile.h"
 
-Automobile::Automobile(string targa, string posizione, double km, u_int numeroPosti, u_int ingombro,
-                       double capacitaSerbatoio, Carburante tipoCarburante, u_int cilindrata, u_int emissioni):
-    Veicolo(targa, posizione, km, numeroPosti, ingombro),
-    MotoreCombustione(targa, posizione, km, numeroPosti, ingombro, capacitaSerbatoio, tipoCarburante, cilindrata, emissioni){}
+Automobile::Automobile(string targa, string posizione, double km, double capacitaSerbatoio, Carburante tipoCarburante, u_int cilindrata, u_int emissioni, u_int numeroPosti, u_int ingombro): Veicolo(targa, posizione, km, numeroPosti, ingombro), MotoreCombustione(targa, posizione, km, numeroPosti, ingombro, capacitaSerbatoio, tipoCarburante, cilindrata, emissioni) {}
