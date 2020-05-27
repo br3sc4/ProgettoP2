03e7d0b6f0eade64c7bb3625c2bd331617b0a252
br3sc4 <lorenzo.brescanzin@icloud.com>
2020-05-27 18:52:19 +0200

    Implementato erase()
    Provalo perché va e non va, nel senso che fa quello che deve fare ma non lo aggiorna sul vettore del contenitore.

--- Model/Array.h
+++ Model/Array.h
@@ -4,21 +4,26 @@
 #include <iostream>
 #include <stdexcept>
 
-class Empty: public std::runtime_error {
+class EmptyException: public std::runtime_error {
 public:
-	Empty(): std::runtime_error("Exception: trying to remove an element from empty array.") {}
+    EmptyException(): std::runtime_error("Exception: trying to remove an element from empty array.") {}
+};
+
+class OutOfBoundsException: public std::out_of_range {
+public:
+    OutOfBoundsException(): std::out_of_range("Exception: index is out of bounds") {}
 };
 
 template <class T>
 class Array;
 template <class T>
-std::ostream& operator<<(std::ostream&, const Array<T>&);
+std::ostream& operator<< (std::ostream&, const Array<T>&);
 
 template <class T>
 class Array {
-	friend std::ostream& operator<<(std::ostream&, const Array<T>&);
+    friend std::ostream& operator<<<T>(std::ostream&, const Array<T>&);
 
- private:
+private:
 	T* x;
 	int _size, _capacity;
 
@@ -29,8 +34,8 @@ class Array {
 		return a;
 	}
 
- public:
-	// costruttore
+public:
+    // costruttore
 	Array(int k = 0, const T& t = T()) : x(0), _size(k), _capacity(k * 2) {
 		if (k) {
 			x = new T[k * 2];
@@ -38,9 +43,10 @@ class Array {
 				x[i] = t;
 		}
 	}
+
 	// costruttore di copia
-	Array(const Array& t)
-			: x(copia(t.x, t._size, t._capacity)), _size(t._size), _capacity(t._capacity) {}
+	Array(const Array& t): x(copia(t.x, t._size, t._capacity)), _size(t._size), _capacity(t._capacity) {}
+
 	// costruttore assegnazione
 	Array& operator=(const Array& t) {
 		if (this != t) {
@@ -51,8 +57,12 @@ class Array {
 		}
 		return *this;
 	}
+
 	// distruttore
-	~Array() { delete[] x; }
+    ~Array() {
+        delete[] x;
+    }
+
 	// operatore di uguaglianza
 	bool operator==(const Array& a) {
 		if (_size != a._size)
@@ -63,7 +73,7 @@ class Array {
 		return true;
 	}
 
-    //****CAPACITY****
+    //===== CAPACITY =====
 
     // Returns the number of elements in the vector.
     int size() const {
@@ -204,48 +214,50 @@ class Array {
         return const_iterator(&x[_size], true);
 	}
 
-	//****ELEMENT ACCESS****
+    //===== ELEMENT ACCESS =====
 
 	// Access element
 	const T& operator[] (int pos) const {
+        if (pos < 0 || pos >= _size) throw OutOfBoundsException();
 		return x[pos];
 	}
 
-	T& operator[] (int pos) {
+    T& operator[] (int pos) {
+        if (pos < 0 || pos >= _size) throw OutOfBoundsException();
 		return x[pos];
 	}
 
 	// Returns a reference to the first element in the vector.
 	T front() const {
 		if(empty())
-				throw Empty();    //undefined behavior
+            throw EmptyException();    //undefined behavior
 		else
 			return x[0];
 	}
 
 	T front() {
 		if(empty())
-				throw Empty();    //undefined behavior
+            throw EmptyException();    //undefined behavior
 		else
 			return x[0];
 	}
 
 	// Returns a reference to the last element in the vector.
-	T back() const {
+    const T back() const {
 		if(empty())
-				throw Empty();    //undefined behavior
+            throw EmptyException();    //undefined behavior
 		else
 			return x[_size-1];
 	}
 
-	T back() {
+    T back() {
 		if(empty())
-				throw Empty();    //undefined behavior
+            throw EmptyException();    //undefined behavior
 		else
 			return x[_size-1];
 	}
 
-	//****MODIFIERS****
+    //===== MODIFIERS =====
 
 	// Add element at the end
 	void push_back(const T& t) {
@@ -262,15 +274,23 @@ class Array {
 			_size--;
 			return aux;
 		} else
-			throw Empty();  // TODO: gestire con una eccezione
+            throw EmptyException();
 	}
 
 	// insert Insert elements (public member function )
 	// TODO: iterator insert (iterator position, const value_type& val);
 
 	// erase Erase elements (public member function )
-	// iterator erase (iterator position);
-	// iterator erase (iterator first, iterator last);
+    iterator erase(iterator position) {
+        --_size;
+        iterator aux = position;
+        while (position != end()) {
+            *position = *(++position);
+            std::cout << *position << " ";
+        }
+        std::cout << std::endl;
+        return aux;
+    }
 	// attenzione metodo inefficiente perché bisogna riallocare gli elementi
 	// ancora presenti
 
@@ -278,9 +298,9 @@ class Array {
 };
 
 template <class T>
-std::ostream& operator<<(std::ostream& os, const Array<T>& a) {
-	for (int i = 0; i < a._size; i++) {
-		os << (a.x)[i] << " ";
+std::ostream& operator<< (std::ostream& os, const Array<T>& a) {
+    for (typename Array<T>::const_iterator it = a.cbegin(); it != a.cend(); it++) {
+        os << *it << " ";
 	}
 	return os;
 }
