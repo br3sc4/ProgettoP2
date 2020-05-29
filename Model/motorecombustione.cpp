24371feb8c79ac83f87271c014b596a1398c7594
archetipo95 <martin.masevski@gmail.com>
2020-05-29 18:47:14 +0200

    Fixed bug su motorecombustione

--- Model/motorecombustione.cpp
+++ Model/motorecombustione.cpp
@@ -2,8 +2,8 @@
 
 
 MotoreCombustione::MotoreCombustione(string targa, string posizione, double km, u_int numeroPosti, u_int ingombro,
-                                     double capacitaSerbatoio, Carburante tipoCarburante, u_int cilindrata, u_int emissioni):
-    Veicolo(targa, posizione, km, numeroPosti, ingombro), _capacitaSerbatoio(capacitaSerbatoio), _litriSerbatoio(capacitaSerbatoio),
+                                     double capacitaSerbatoio,double litriSerbatoio, Carburante tipoCarburante, u_int cilindrata, u_int emissioni):
+    Veicolo(targa, posizione, km, numeroPosti, ingombro), _capacitaSerbatoio(capacitaSerbatoio), _litriSerbatoio(litriSerbatoio),
     _tipoCarburante(tipoCarburante), _cilindrata(cilindrata), _emissioni(emissioni) {}
 
 MotoreCombustione::~MotoreCombustione() {};
--- Model/motorecombustione.h
+++ Model/motorecombustione.h
@@ -11,31 +11,28 @@ public:
 
     /**
      * @brief MotoreCombustione: Costruttore
-     * @param targa: la targa del veicolo
-     * @param posizione: la posizione attuale del veicolo
-     * @param km: i chilometri percorsi dal veicolo
-     * @param numeroPosti: i posti per cui è omologato il veicolo
-     * @param ingombro: le dimensioni del veicolo
+     *
+     * @param from Veicolo(targa, posizione, km, numeroPosti, ingombro)
      * @param capacitaSerbatoio: quanti litri di carburante può contenere il serbatoio del veicolo
+     * @param litriSerbatoio: quanti litri di carburante sono rimasti
      * @param tipoCarburante: il carburante utilizzato dal veicolo
      * @param cilindrata: la cilindrata del motore
      * @param emissioni: le emissioni dinquinanti del veicolo
      */
     MotoreCombustione(string targa, string posizione, double km, u_int numeroPosti, u_int ingombro,
-                      double capacitaSerbatoio, Carburante tipoCarburante, u_int cilindrata, u_int emissioni);
+                      double capacitaSerbatoio, double litriSerbatoio, Carburante tipoCarburante, u_int cilindrata, u_int emissioni);
 
     /**
      * @brief ~MotoreCombustione: Distruttore
      */
     virtual ~MotoreCombustione();
 
-
 private:
     double _capacitaSerbatoio;
     double _litriSerbatoio;
-    Carburante _tipoCarburante;     // 0 = benzina, 1 = diesel, 2 = GPL, 3 = Metano
+    Carburante _tipoCarburante;
     u_int _cilindrata;
-    u_int _emissioni;               //  da 0 a 95g circa per km
+    u_int _emissioni;
 };
 
 #endif // MOTORECOMBUSTIONE_H
