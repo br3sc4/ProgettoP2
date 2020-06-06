346407e5cc7ceb36a8ac911b6d585c637cce7348
archetipo95 <martin.masevski@gmail.com>
2020-06-06 10:56:46 +0200

    add 2 nuovi parametri su MotoreElettrico

--- MODELS/motoreelettrico.h
+++ MODELS/motoreelettrico.h
@@ -21,7 +21,7 @@ public:
      * @param caricaSupportata: la velocità di carica a cui può essere caricato il veicolo
      */
     MotoreElettrico(string targa, string posizione, double km, u_int numeroPosti, u_int ingombro,
-                    bool inCarica, double capacitaBatteria, double capacitaAttuale, VelocitaRicarica caricaSupportata, Colonnina colonninaAttuale);
+                    u_int potenza, u_int numeroMotori, bool inCarica, double capacitaBatteria, double capacitaAttuale, VelocitaRicarica caricaSupportata, Colonnina colonninaAttuale);
 
     virtual ~MotoreElettrico();
 
@@ -48,7 +48,9 @@ public:
     void setCapacitaAttuale(double capacitaAttuale);
 
 private:
-    bool _inCarica;
+    const u_int _potenza;                       // Potenza del motore
+    const u_int _numeroMotori;                  // Il numero di motori presenti (da 1 a 4)
+    bool _inCarica;                             // E' in carica ?
     const double _capacitaBatteria;             // Ampere-ora (Ah) massimi
     double _capacitaAttuale;                    // Ampere-ora presenti
     const VelocitaRicarica _caricaSupportata;   // la massima velocità alla quale può essere ricaricata (da 1 a 4)
