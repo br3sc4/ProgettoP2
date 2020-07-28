4d5ecc705214007a0e275948f0d6cd64992b46cf
archetipo95 <martin.masevski@gmail.com>
2020-07-28 13:31:03 +0200

    Aggiunto db con 30 biciclette

--- main.cpp
+++ main.cpp
@@ -23,11 +23,20 @@ int main(int argc, char *argv[])
     }else std::cout << "non riesco ad aprire il file dBCitta\n";
     dBCitta.close();
 
-    model.addVehicle(0, new Bicicletta("Targa-Bici1", "posizione", 10, 10, 10, 2000, 1000, Bicicletta::veloce));
-    model.addVehicle(0, new Automobile("Targa-Auto1", "pos", 100, 60, 20, Automobile::diesel, 1600, 60, 4));
-    model.addVehicle(1, new Bicicletta("Targa-Bici2", "posizione", 10, 10, 10, 2000, 1000, Bicicletta::veloce));
-    model.addVehicle(1, new Automobile("Targa-Auto2", "pos", 100, 60, 20, Automobile::diesel, 1600, 60, 4));
-    model.addVehicle(1, new Automobile("Targa-Auto3", "pos", 100, 60, 20, Automobile::diesel, 1600, 60, 4));
+    std::ifstream dBBiciclette("database/dataBaseBiciclette.txt");
+    if (dBBiciclette.is_open()){
+        int citta, km, potenza, nMotori, velocita;
+        double capBatt, capAttuale;
+        string targa, posizione;
+        while(dBBiciclette >> citta >> targa >> posizione >> km >> potenza >> nMotori >> capBatt >> capAttuale >> velocita){
+            model.addVehicle(citta, new Bicicletta(targa, posizione, km, potenza, nMotori, capBatt, capAttuale, static_cast<Bicicletta::VelocitaRicarica>(velocita)));
+        }
+    }else std::cout << "non riesco ad aprire il file dBBiciclette\n";
+    dBBiciclette.close();
+
+    //model.addVehicle(0, new Automobile("Targa-Auto1", "pos", 100, 60, 20, Automobile::diesel, 1600, 60, 4));
+    //model.addVehicle(1, new Automobile("Targa-Auto2", "pos", 100, 60, 20, Automobile::diesel, 1600, 60, 4));
+    //model.addVehicle(1, new Automobile("Targa-Auto3", "pos", 100, 60, 20, Automobile::diesel, 1600, 60, 4));
 
     Controller controller(&model);
     View w(&controller);
