f1422e1da0851a369724ba8749dcb2fcdeba1d47
archetipo95 <martin.masevski@gmail.com>
2020-07-30 11:43:52 +0200

    mezzo fix

--- VIEWS/vehiclelistview.cpp
+++ VIEWS/vehiclelistview.cpp
@@ -16,8 +16,8 @@ void VehicleListView::update() {
         _table->setCellWidget(i, 1, icon);
         item = new QTableWidgetItem(QString::fromStdString(vehicles[i]->posizione()));
         _table->setItem(i, 2, item);
-        item = new QTableWidgetItem(QString::number(vehicles[i]->chilometraggio()));
-        _table->setItem(i, 3, item);
+        //item = new QTableWidgetItem(QString::number(vehicles[i]->chilometraggio()));
+        //_table->setItem(i, 3, item);
 
         connect(_table, &QTableWidget::itemClicked, this, [=](QTableWidgetItem* item) {
             emit rowClicked(item->row());
@@ -28,18 +28,18 @@ void VehicleListView::update() {
 const QString& VehicleListView::getIconPath(Veicolo* const vehicle) {
     QString basePath = ":/icons/";
 
-//    if (typeid(vehicle) == typeid(Automobile))
+    if (typeid(vehicle) == typeid(Automobile))
         return basePath += "auto.png";
-//    else if (typeid(vehicle) == typeid(AutoIbrida))
-//        return basePath += "auto_ibrida.png";
-//    else if (typeid(vehicle) == typeid(AutomobileElettrica))
-//        return basePath += "auto_elettrica.png";
-//    else if (typeid(vehicle) == typeid(Moto))
-//        return basePath+= "moto.png";
-//    else if (typeid(vehicle) == typeid(MotoElettrica))
-//        return basePath += "moto_elettrica.png";
-//    else if (typeid(vehicle) == typeid(Bicicletta))
-//        return basePath += "bici.png";
-//    else
-//        return basePath += "monopattino.png";
+    else if (typeid(vehicle) == typeid(AutoIbrida))
+        return basePath += "auto_ibrida.png";
+    else if (typeid(vehicle) == typeid(AutomobileElettrica))
+        return basePath += "auto_elettrica.png";
+    else if (typeid(vehicle) == typeid(Moto))
+        return basePath+= "moto.png";
+    //else if (typeid(vehicle) == typeid(MotoElettrica))
+        //return basePath += "moto_elettrica.png";
+    else if (typeid(vehicle) == typeid(Bicicletta))
+        return basePath += "bici.png";
+    else
+        return basePath += "monopattino.png";
 }
