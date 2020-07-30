35f36900153598c4469f345c3e8fd445ba130997
br3sc4 <lorenzo.brescanzin@icloud.com>
2020-07-30 11:23:15 +0200

    Refactor

--- VIEWS/vehiclelistview.cpp
+++ VIEWS/vehiclelistview.cpp
@@ -30,16 +30,16 @@ const QString& VehicleListView::getIconPath(Veicolo* const vehicle) {
 
     if (typeid(vehicle) == typeid(Automobile))
         return basePath += "auto.png";
-    else if (typeid(vehicle) == typeid(AutoIbrida))
-        return basePath += "auto_ibrida.png";
-    else if (typeid(vehicle) == typeid(AutomobileElettrica))
-        return basePath += "auto_elettrica.png";
-    else if (typeid(vehicle) == typeid(Moto))
-        return basePath+= "moto.png";
+//    else if (typeid(vehicle) == typeid(AutoIbrida))
+//        return basePath += "auto_ibrida.png";
+//    else if (typeid(vehicle) == typeid(AutomobileElettrica))
+//        return basePath += "auto_elettrica.png";
+//    else if (typeid(vehicle) == typeid(Moto))
+//        return basePath+= "moto.png";
 //    else if (typeid(vehicle) == typeid(MotoElettrica))
 //        return basePath += "moto_elettrica.png";
-    else if (typeid(vehicle) == typeid(Bicicletta))
-        return basePath += "bici.png";
-    else
-        return basePath += "monopattino.png";
+//    else if (typeid(vehicle) == typeid(Bicicletta))
+//        return basePath += "bici.png";
+//    else
+//        return basePath += "monopattino.png";
 }
