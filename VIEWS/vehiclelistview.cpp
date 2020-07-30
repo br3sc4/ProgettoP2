875b3dddcef9012f10bbd5597dabb468d660f5f1
br3sc4 <lorenzo.brescanzin@icloud.com>
2020-07-30 12:32:37 +0200

    Update vehiclelistview.cpp

--- VIEWS/vehiclelistview.cpp
+++ VIEWS/vehiclelistview.cpp
@@ -13,7 +13,7 @@ void VehicleListView::update() {
         QString iconPath = getIconPath(*vehicles[i]);
         std::cout << iconPath.toStdString() << std::endl;
 //        icon->setPixmap(QPixmap());
-        _table->setCellWidget(i, 0, icon);
+//        _table->setCellWidget(i, 0, icon);
         QTableWidgetItem *item = new QTableWidgetItem(QString::fromStdString(vehicles[i]->targa()));
         _table->setItem(i, 1, item);
         item = new QTableWidgetItem(QString::fromStdString(vehicles[i]->posizione()));
@@ -30,19 +30,25 @@ void VehicleListView::update() {
 const QString& VehicleListView::getIconPath(const Veicolo& vehicle) {
     QString basePath = ":/icons/";
 
-    std::cout << typeid(vehicle).name() << " " << typeid(Automobile).name() << std::endl;
-//    if (typeid(vehicle) == typeid(Automobile))
+    std::cout << typeid(vehicle).name() << std::endl;
+    if (typeid(vehicle) == typeid(Automobile))
+        std::cout << typeid(vehicle).name() << " " << typeid(Automobile).name() << std::endl;
 //        return basePath += "auto.png";
-//    else if (typeid(vehicle) == typeid(AutoIbrida))
+    else if (typeid(vehicle) == typeid(AutoIbrida))
+        std::cout << typeid(vehicle).name() << " " << typeid(AutoIbrida).name() << std::endl;
 //        return basePath += "auto_ibrida.png";
-//    else if (typeid(vehicle) == typeid(AutomobileElettrica))
+    else if (typeid(vehicle) == typeid(AutomobileElettrica))
+        std::cout << typeid(vehicle).name() << " " << typeid(AutomobileElettrica).name() << std::endl;
 //        return basePath += "auto_elettrica.png";
-//    else if (typeid(vehicle) == typeid(Moto))
+    else if (typeid(vehicle) == typeid(Moto))
+        std::cout << typeid(vehicle).name() << " " << typeid(Moto).name() << std::endl;
 //        return basePath+= "moto.png";
-////    else if (typeid(vehicle) == typeid(MotoElettrica))
-////        return basePath += "moto_elettrica.png";
-//    else if (typeid(vehicle) == typeid(Bicicletta))
+//    else if (typeid(vehicle) == typeid(MotoElettrica))
+//        return basePath += "moto_elettrica.png";
+    else if (typeid(vehicle) == typeid(Bicicletta))
+        std::cout << typeid(vehicle).name() << " " << typeid(Bicicletta).name() << std::endl;
 //        return basePath += "bici.png";
-//    else
+    else
+        std::cout << typeid(vehicle).name() << " " << typeid(Monopattino).name() << std::endl;
 //        return basePath += "monopattino.png";
 }
